2ff150be6ec5306e5e5c10567548d1ebff266238
Nolan Miller <nolan.miller77@gmail.com>
2025-06-08 15:40:27 -0400

    remove build files

--- QuidditchTrip.Models/obj/Debug/net9.0/QuidditchTrip.Models.AssemblyInfo.cs
+++ QuidditchTrip.Models/obj/Debug/net9.0/QuidditchTrip.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuidditchTrip.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4089de7a3c7e90a3e731a834fc5269edf3f6ce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuidditchTrip.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuidditchTrip.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
